51460bfb17de44f9e62b65f6316f1085fe555803
Johngtka <johngtka@gmail.com>
2023-06-03 15:30:17 +0200

    feat: added new methods of routes

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET, POST");
 
 $config = require_once 'dbconnect.php';
 $users = require_once './routes/users.php';
 
-try{
+try {
   $db = new PDO("mysql:host={$config['host']};dbname={$config['db']};charset=utf8", $config['user'], $config['pass'], [PDO::ATTR_EMULATE_PREPARES => false, PDO::ERRMODE_EXCEPTION]);
-  if($_SERVER['REQUEST_METHOD']==='GET'){
+
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $users.getUsers();
   }
-} catch(PDOException $error){
-  echo json_encode(['error' => 'Błąd połączenia z bazą danych']);
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $requestData = json_decode(file_get_contents('php://input'), true);
+
+    if (isset($requestData['name']) && isset($requestData['surName']) && isset($requestData['dob'])) {
+      $data = [
+        'name' => $requestData['name'],
+        'surName' => $requestData['surName'],
+        'dob' => $requestData['dob']
+      ];
+      $users.postUser($data);
+    }
+    
+    if(isset($requestData['name'])){
+    $data = [
+      'name' => $requestData['name']
+    ];
+    $users.searchUsers($data);
+    }else{
+      echo json_encode("xd");
+    }
+  }
+} catch (PDOException $error) {
+  echo http_response_code(500);
 }
+?>
--- routes/users.php
+++ routes/users.php
@@ -6,4 +6,20 @@ function getUsers(){
     $result = $query->fetchAll();
     echo json_encode($result);
 }
+function postUser($data){
+    global $db;
+    $query = $db->prepare("INSERT INTO workers VALUES (NULL, :name, :surName, :dob)");
+    $query->bindParam(':name', $data['name']);
+    $query->bindParam(':surName', $data['surName']);
+    $query->bindParam(':dob', $data['dob']);
+    $query->execute();
+}
 
+function searchUsers($data){
+    global $db;
+    $query = $db->prepare("SELECT name, surName FROM workers WHERE name=:imie");
+    $query->bindParam(':imie',  $data['name']);
+    $query->execute();
+    $result = $query->fetchAll();
+    echo json_encode($result);
+}
