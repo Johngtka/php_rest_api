210908d6517081a3fddd52fea8b0880a59e78dea
Johngtka <johngtka@gmail.com>
2023-06-05 12:15:38 +0200

    refactor: moved all of endpoints http request to separate function

--- index.php
+++ index.php
@@ -1,51 +1,17 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header("Access-Control-Allow-Methods: GET, POST");
 
-$config = require_once 'dbconnect.php';
-$users = require_once './routes/workers.php';
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header("Access-Control-Allow-Methods: GET, POST");
 
-try {
-  $db = new PDO("mysql:host={$config['host']};dbname={$config['db']};charset=utf8", $config['user'], $config['pass'], [PDO::ATTR_EMULATE_PREPARES => false, PDO::ERRMODE_EXCEPTION]);
+  $app = require_once './api.php';
+  $config = require_once './dbConnect.php';
 
-  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // getting all employers
-    $users.getWorkers();
-  }
-
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $requestData = json_decode(file_get_contents('php://input'), true);
-
-    // new employee
-    if (isset($requestData['name']) && isset($requestData['surName']) && isset($requestData['dob'])) {
-        $data = [
-          'name' => $requestData['name'],
-          'surName' => $requestData['surName'],
-          'dob' => $requestData['dob']
-        ];
-        $users.postWorker($data);
-    }
-    
-    // search employee
-    if(isset($requestData['name'])){
-        $data = [
-          'name' => $requestData['name']
-        ];
-        $users.searchWorkers($data);
-    }else{
-        echo json_encode("xd");
+    try {
+        $db = new PDO("mysql:host={$config['host']};dbname={$config['db']};charset=utf8", $config['user'], $config['pass'], [PDO::ATTR_EMULATE_PREPARES => false, PDO::ERRMODE_EXCEPTION]);
+        $app.API();
+    } catch (PDOException $error) {
+        echo $error->getCode();
     }
 
-    if(isset($requestData['del']) && $requestData['del']===true){
-        $data = [
-          'name'=> $requestData['name'] 
-        ];
-        $users.deleteWorker($data);
-    };
-    }
-} catch (PDOException $error) {
-   echo http_response_code(500);
-}
 ?>
--- routes/workers.php
+++ routes/workers.php
@@ -1,39 +1,38 @@
 <?php
-function getWorkers(){
-    global $db;
-    $query = $db->prepare("SELECT * FROM workers");
-    $query->execute();
-    $result = $query->fetchAll();
-    echo json_encode($result);
-}
-function postWorker($data){
-    global $db;
-    $query = $db->prepare("INSERT INTO workers VALUES (NULL, :name, :surName, :dob)");
-    $query->bindParam(':name', $data['name']);
-    $query->bindParam(':surName', $data['surName']);
-    $query->bindParam(':dob', $data['dob']);
-    $query->execute();
-}
 
-function searchWorkers($data){
-    global $db;
-    $query = $db->prepare("SELECT name, surName FROM workers WHERE name=:imie");
-    $query->bindParam(':imie',  $data['name']);
-    $query->execute();
-    $result = $query->fetchAll();
-    echo json_encode($result);
-}
+    function getWorkers(){
+        global $db;
+        $query = $db->prepare("SELECT * FROM workers");
+        $query->execute();
+        $result = $query->fetchAll();
+        echo json_encode($result);
+    }
 
+    function postWorker($data){
+        global $db;
+        $query = $db->prepare("INSERT INTO workers VALUES (NULL, :name, :surName, :dob)");
+        $query->bindParam(':name', $data['name']);
+        $query->bindParam(':surName', $data['surName']);
+        $query->bindParam(':dob', $data['dob']);
+        $query->execute();
+    }
 
-function deleteWorker($data){
-    global $db;
-    $query = $db->prepare("DELETE FROM workers WHERE name=:imie");
-    $query->bindParam(':imie', $data['name']);
-    $query->execute();
-    $query2 = $db->prepare("ALTER TABLE workers AUTO_INCREMENT=1");
-    $query2->execute();
-    $query3 = $db->prepare("ALTER TABLE workers DROP id");
-    $query3->execute();
-    $addid = $db->prepare("ALTER TABLE workers ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`)");
-    $addid->execute();
-}
+    function searchWorkers($data){
+        global $db;
+        $query = $db->prepare("SELECT name, surName FROM workers WHERE name=:imie");
+        $query->bindParam(':imie',  $data['name']);
+        $query->execute();
+        $result = $query->fetchAll();
+        echo json_encode($result);
+    }
+
+    function deleteWorker($data){
+        global $db;
+        $query = $db->prepare("DELETE FROM workers WHERE name=:imie");
+        $query->bindParam(':imie', $data['name']);
+        $query->execute();
+        $query3 = $db->prepare("ALTER TABLE workers DROP id");
+        $query3->execute();
+        $addid = $db->prepare("ALTER TABLE workers ADD `id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`)");
+        $addid->execute();
+    }
